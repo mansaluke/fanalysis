741a1f0f875608e71efddb3cc10116f1b47b2151
mansaluke <lmcleary@s2g.deloitte.es>
2019-05-24 18:51:42 +0200

    general update

---
+++ fanalysis/timeseries.py
@@ -0,0 +1,19 @@
+x=2
+
+from dfconvert import df_store
+import extract
+
+import pandas as pd 
+import scipy
+
+if x == 1:
+    import main
+    df = dfC.storage_to_df('x.json').file
+    print(df.head())
+elif x == 2: 
+    df = e.use_csvs()
+
+
+
+from scipy import linalg
+
